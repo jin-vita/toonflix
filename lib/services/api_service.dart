e4d786ab02da91619d7b4579269e72b9d18e1b0a
VITA-R9 <jhj9144@gmail.com>
2023-02-04 08:01:54 +0900

    backup

---
+++ lib/services/api_service.dart
@@ -0,0 +1,8 @@
+/// 네이버 웹툰 Unofficial API
+/// /today: Returns today's comics (kids friendly).
+/// /:id: Returns a comic's information by `:id`
+/// /:id/episodes: Returns the latest episodes for a comic by `:id`
+class ApiService {
+  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  final String today = 'today';
+}
